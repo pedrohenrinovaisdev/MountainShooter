0ddff57e48060a5fed44fbb8c8ea8b30651362a7
Pedro_Henri <pedro123mobile@gmail.com>
2026-06-15 20:45:00 -0300

    Add of menu image and menu sound

--- code/game.py
+++ code/game.py
@@ -2,23 +2,20 @@
 # -*- coding: utf-8 -*-
 import pygame
 from code.menu import Menu
+from const import WIN_WIDTH, WIN_HEIGHT
+
 
 class Game:
 
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 500))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self):
+    def run(self, ):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # Close window
-            #         quit()  # End pygame
 
 
 
